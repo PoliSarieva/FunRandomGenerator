4fafbd653335b6e64786d487d49cd356e1181943
PoliSarieva <109367470+PoliSarieva@users.noreply.github.com>
2022-10-27 23:35:40 +0300

    RandomSentencesGenerator

---
+++ RandomSentencesGenerator.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomSentencesGenerator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = {"Peter", "Michell", "Jane", "Steve"};
+        String[] places = {"Sofia", "Plovdiv", "Varna", "Burgas"};
+        String[] verbs = {"eats", "holds", "sees", "plays with", "brings"};
+        String[] nouns = {"stones", "cake", "apple", "laptop", "bikes"};
+        String[]  adverbs = {"slowly", "diligently", "warmly", "sadly", "rapidly"};
+        String[]  details = {"near the river", "at home", "in the park"};
+    }
+
+    public static String getRandomWord (String [] words) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length);
+        String word = words[randomIndex];
+        return word;
+    }
+}
